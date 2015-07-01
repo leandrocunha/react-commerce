7085ef19e12c7bf9911a98fa4a5fa6b6a13a589d
Christian Amaral <darthcas@gmail.com>
2015-07-01 10:09:46 -0300

    owl carousel funcional

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 import $ from 'jquery'
 import Page from './components/page.js'
 
-LazyLoad.js([`shared/vendors/owl.carousel/src/js/owl.carousel.js`], () => {
+global.$ = global.jQuery = $;
 
-  global.jQuery = require('jquery');
-  global.$ = jQuery;
-  console.log('foo.js has been loaded');
+console.log('foo.js has been loaded');
 
-  React.render(
-    <Page />,
-    document.getElementById('app')
-  );
-
-});
+React.render(
+	<Page />,
+	document.getElementById('app')
+);
--- components/featured-carousel.js
+++ components/featured-carousel.js
@@ -1,27 +1,36 @@
 export default class FeaturedCarousel extends React.Component {
 
-  componentDidMount(){
-    LazyLoad.js([`shared/vendors/owl.carousel/src/js/owl.carousel.js`], () => {
-      $(React.findDOMNode(this.refs.featuredCarousel)).owlCarousel();
-    });
-  }
+	componentDidMount() {
+		let loadOwljs = () =>
+			LazyLoad.js(
+				['shared/vendors/owl.carousel/src/js/owl.carousel.js'],
+				() => $(React.findDOMNode(this.refs.featuredCarousel)).owlCarousel({
+					singleItem: true
+				})
+			);
 
-  render(){
+		LazyLoad.css([
+			'shared/vendors/owl.carousel/src/css/owl.carousel.css',
+			'shared/vendors/owl.carousel/src/css/owl.theme.default.css'
+		], loadOwljs);
+	}
 
-    return(
-      <div className="featured-carousel" ref="featuredCarousel">
-        <ul className="slides-list">
-          <li className="slide">
-            <img src="assets/img/slide1.jpg" />
-          </li>
-          <li className="slide">
-            <img src="assets/img/slide2.jpg" />
-          </li>
-          <li className="slide">
-            <img src="assets/img/slide3.jpg" />
-          </li>
-        </ul>
-      </div>
-    );
-  }
+	render() {
+
+		return (
+			<div className='featured-carousel'>
+				<div className='slides-list owl-carousel' ref='featuredCarousel'>
+					<div className='slide'>
+						<img src='assets/img/slide1.jpg'/>
+					</div>
+					<div className='slide'>
+						<img src='assets/img/slide2.jpg'/>
+					</div>
+					<div className='slide'>
+						<img src='assets/img/slide3.jpg'/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
