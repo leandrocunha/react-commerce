f37ab16b15c1040253faf44752f07b5a3c4e5a0d
Frango <leandroscunha@gmail.com>
2015-07-27 17:53:08 -0300

    Start image upload

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,14 +14,14 @@ router.get('/helloworld', function(req, res) {
 });
 
 router.get('/tshirts', function(req, res) {
-    var db = req.db;
-    var collection = db.get('productcollection');
-    collection.find({},{},function(e,docs){
-        res.render('products', {
-            "title": "Products",
-            "tshirts" : docs
-        });
-    });
+  var db = req.db;
+  var collection = db.get('productcollection');
+  collection.find({},{},function(e,docs){
+      res.render('products', {
+          "title": "Products",
+          "tshirts" : docs
+      });
+  });
 });
 
 router.get('/tshirts/new', function(req, res) {
@@ -28,29 +30,79 @@ router.get('/tshirts/new', function(req, res) {
 
 /* POST to Add User Service */
 router.post('/addproduct', function(req, res) {
+  
+  var db = req.db;
+  var collection = db.get('productcollection');
+  var form = new formidable.IncomingForm();
 
-    // Set our internal DB variable
-    var db = req.db;
+  form.parse(req, function(err, fields, files) {
+    console.log(fields);
 
-    // Get our form values. These rely on the "name" attributes
-    var productName = req.body.name;
+    // res.writeHead(200, {'content-type': 'text/plain'});
+    // res.write('received upload:\n\n');
+    // var image = files.image
+    //   , image_upload_path_old = image.path
+    //   , image_upload_path_new = './public/images/'
+    //   , image_upload_name = image.name
+    //   , image_upload_path_name = image_upload_path_new + image_upload_name
+    //   ;
 
-    // Set our collection
-    var collection = db.get('productcollection');
+    // if (fs.existsSync(image_upload_path_new)) {
+    //   fs.rename(
+    //     image_upload_path_old,
+    //     image_upload_path_name,
+    //     function (err) {
+    //     if (err) {
+    //       console.log('Err: ', err);
+    //       res.end('Deu merda na hora de mover a imagem!');
+    //     }
+    //     var msg = 'Imagem ' + image_upload_name + ' salva em: ' + image_upload_path_new;
+    //     console.log(msg);
+    //     res.end(msg);
+    //   });
+    // }
+    // else {
+    //   fs.mkdir(image_upload_path_new, function (err) {
+    //     if (err) {
+    //       console.log('Err: ', err);
+    //       res.end('Deu merda na hora de criar o diretório!');
+    //     }
+    //     fs.rename(
+    //       image_upload_path_old,
+    //       image_upload_path_name,
+    //       function(err) {
+    //       var msg = 'Imagem ' + image_upload_name + ' salva em: ' + image_upload_path_new;
+    //       console.log(msg);
+    //       res.end(msg);
+    //     });
+    //   });
+    // }
+  });
 
-    // Submit to the DB
-    collection.insert({
-        "name" : productName
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // And forward to success page
-            res.redirect("tshirts");
-        }
-    });
+  // collection.insert({
+  //     "name" : productName
+  // }, function (err, doc) {
+  //     if (err) {
+  //         // If it failed, return error
+  //         res.send("There was a problem adding the information to the database.");
+  //     }
+  //     else {
+  //         // And forward to success page
+  //         res.redirect("tshirts");
+  //     }
+  // });    
+
+});
+
+module.exports = router;
+
+
+
+
+router.post('/upload', function (req, res) {
+  var form = new formidable.IncomingForm();
+
+
 });
 
 module.exports = router;
