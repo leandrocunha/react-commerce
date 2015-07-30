5e9b4f82f7a378dab2466aef69f8618822c0e507
Frango <leandroscunha@gmail.com>
2015-07-30 08:34:26 -0300

    Start authentication

--- admin/app.js
+++ admin/app.js
@@ -6,7 +6,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
+var exphbs  = require('express-handlebars'),
+    passport = require('passport'),
+    util = require('util'),
+    DigestStrategy = require('passport-http').DigestStrategy
 
 var mongo = require('mongodb');
 var monk = require('monk');
@@ -29,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
@@ -36,10 +40,62 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use('/', routes);
+// curl -v -I http://127.0.0.1:3000/
+// curl -v -I --user bob:secret --digest http://127.0.0.1:3000/
+// curl -v -d "hello=world" --user bob:secret --digest http://127.0.0.1:3000/
+app.all('/',
+  // Authenticate using HTTP Digest credentials, with session support disabled.
+  passport.authenticate('digest', { session: false }),
+  function(req, res){
+    res.json({ username: req.user.username, email: req.user.email });
+  });
 app.use('/users', users);
 app.use('/tshirts', tshirts);
 app.use('/product', tshirts);
+
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+// Use the DigestStrategy within Passport.
+//   This strategy requires a `secret`function, which is used to look up the
+//   use and the user's password known to both the client and server.  The
+//   password is used to compute a hash, and authentication will fail if the
+//   computed value does not match that of the request.  Also required is a
+//   `validate` function, which can be used to validate nonces and other
+//   authentication parameters contained in the request.
+passport.use(new DigestStrategy({ qop: 'auth' },
+  function(username, done) {
+    // Find the user by username.  If there is no user with the given username
+    // set the user to `false` to indicate failure.  Otherwise, return the
+    // user and user's password.
+    findByUsername(username, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, user.password);
+    })
+  },
+  function(params, done) {
+    // asynchronous validation, for effect...
+    process.nextTick(function () {
+      // check nonces in params here, if desired
+      return done(null, true);
+    });
+  }
+));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-var formidable = require('formidable');
-var _ = require('lodash');
+var express = require('express'),
+    router = express.Router(),
+    fs = require('fs'),
+    formidable = require('formidable'),
+    _ = require('lodash');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
